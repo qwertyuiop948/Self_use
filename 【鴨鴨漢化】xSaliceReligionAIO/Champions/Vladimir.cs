b318fdb223fe95676cd77f9595f04f71abd79b39
qwertyuiop948 <qwertyuiop948@yahoo.com.tw>
2014-12-09 07:06:21 +0800

    新增漢化(未完成)

---
+++ 【鴨鴨漢化】xSaliceReligionAIO/Champions/Vladimir.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+
+namespace xSaliceReligionAIO.Champions
+{
+    class Vladimir : Champion
+    {
+        public Vladimir()
+        {
+            LoadSpell();
+            LoadMenu();
+        }
+
+        private void LoadSpell()
+        {
+            Q = new Spell(SpellSlot.Q, 600);
+            W = new Spell(SpellSlot.W);
+            E = new Spell(SpellSlot.E, 575);
+            R = new Spell(SpellSlot.R, 700);
+
+            R.SetSkillshot(0.25f, 175, 700, false, SkillshotType.SkillshotCircle);
+        }
+
+        private void LoadMenu()
+        {
+            var key = new Menu("Key", "Key");
+            {
+                key.AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+                key.AddItem(new MenuItem("HarassActive", "Harass!").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+                key.AddItem(new MenuItem("HarassActiveT", "Harass (toggle)!").SetValue(new KeyBind("N".ToCharArray()[0], KeyBindType.Toggle)));
+                key.AddItem(new MenuItem("LaneClearActive", "Farm!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+                key.AddItem(new MenuItem("LastHitKey", "Last Hit!").SetValue(new KeyBind("A".ToCharArray()[0], KeyBindType.Press)));
+                key.AddItem(new MenuItem("StackE", "StackE (toggle)!").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Toggle)));
+                //add to menu
+                menu.AddSubMenu(key);
+            }
+
+            var combo = new Menu("Combo", "Combo");
+            {
+                combo.AddItem(new MenuItem("selected", "Focus Selected Target").SetValue(true));
+                combo.AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+                combo.AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
+                combo.AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+                combo.AddItem(new MenuItem("UseRCombo", "Use R").SetValue(true));
+                combo.AddItem(new MenuItem("Ignite", "Use Ignite").SetValue(true));
+                combo.AddItem(new MenuItem("igniteMode", "Ignite Mode").SetValue(new StringList(new[] { "Combo", "KS" })));
+                //add to menu
+                menu.AddSubMenu(combo);
+            }
+
+            var harass = new Menu("Harass", "Harass");
+            {
+                harass.AddItem(new MenuItem("UseQHarass", "Use Q").SetValue(true));
+                harass.AddItem(new MenuItem("UseEHarass", "Use E").SetValue(true));
+                //add to menu
+                menu.AddSubMenu(harass);
+            }
+
+            var farm = new Menu("LaneClear", "LaneClear");
+            {
+                farm.AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(true));
+                farm.AddItem(new MenuItem("UseEFarm", "Use E").SetValue(true));
+                //add to menu
+                menu.AddSubMenu(farm);
+            }
+
+            var miscMenu = new Menu("Misc", "Misc");
+            {
+                miscMenu.AddItem(new MenuItem("W_Gap_Closer", "Use W On Gap Closer").SetValue(true));
+                miscMenu.AddItem(new MenuItem("useR_Hit", "Use R if hit, 0 = off").SetValue(new Slider(3, 0, 5)));
+                miscMenu.AddItem(new MenuItem("smartKS", "Smart KS").SetValue(true));
+                miscMenu.AddItem(new MenuItem("R_KS", "Use R to KS").SetValue(true));
+                //add to menu
+                menu.AddSubMenu(miscMenu);
+            }
+
+            var drawMenu = new Menu("Drawing", "Drawing");
+            {
+                drawMenu.AddItem(new MenuItem("Draw_Disabled", "Disable All").SetValue(false));
+                drawMenu.AddItem(new MenuItem("Draw_Q", "Draw Q").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_E", "Draw E").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_R", "Draw R").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_R_Killable", "Draw R Mark on Killable").SetValue(true));
+
+                MenuItem drawComboDamageMenu = new MenuItem("Draw_ComboDamage", "Draw Combo Damage").SetValue(true);
+                MenuItem drawFill = new MenuItem("Draw_Fill", "Draw Combo Damage Fill").SetValue(new Circle(true, Color.FromArgb(90, 255, 169, 4)));
+                drawMenu.AddItem(drawComboDamageMenu);
+                drawMenu.AddItem(drawFill);
+                DamageIndicator.DamageToUnit = GetComboDamage;
+                DamageIndicator.Enabled = drawComboDamageMenu.GetValue<bool>();
+                DamageIndicator.Fill = drawFill.GetValue<Circle>().Active;
+                DamageIndicator.FillColor = drawFill.GetValue<Circle>().Color;
+                drawComboDamageMenu.ValueChanged +=
+                    delegate(object sender, OnValueChangeEventArgs eventArgs)
+                    {
+                        DamageIndicator.Enabled = eventArgs.GetNewValue<bool>();
+                    };
+                drawFill.ValueChanged +=
+                    delegate(object sender, OnValueChangeEventArgs eventArgs)
+                    {
+                        DamageIndicator.Fill = eventArgs.GetNewValue<Circle>().Active;
+                        DamageIndicator.FillColor = eventArgs.GetNewValue<Circle>().Color;
+                    };
+                //add to menu
+                menu.AddSubMenu(drawMenu);
+            }
+        }
+
+        private float GetComboDamage(Obj_AI_Base target)
+        {
+            double comboDamage = 0;
+
+            if (Q.IsReady())
+                comboDamage += Player.GetSpellDamage(target, SpellSlot.Q);
+
+            if (E.IsReady())
+                comboDamage += Player.GetSpellDamage(target, SpellSlot.E);
+
+            if (R.IsReady())
+            {
+                comboDamage += Player.GetSpellDamage(target, SpellSlot.R);
+                comboDamage += comboDamage * 1.12;
+            }
+            else if (target.HasBuff("vladimirhemoplaguedebuff", true))
+            {
+                comboDamage += comboDamage * 1.12;
+            }
+
+            if (Ignite_Ready())
+                comboDamage += Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite);
+
+            return (float)(comboDamage + Player.GetAutoAttackDamage(target));
+        }
+
+        private void Combo()
+        {
+            UseSpells(menu.Item("UseQCombo").GetValue<bool>(),
+                menu.Item("UseECombo").GetValue<bool>(), menu.Item("UseRCombo").GetValue<bool>());
+        }
+
+        private void Harass()
+        {
+            UseSpells(menu.Item("UseQHarass").GetValue<bool>(),
+                menu.Item("UseEHarass").GetValue<bool>(), false);
+        }
+
+        private void UseSpells(bool useQ, bool useE, bool useR)
+        {
+            int igniteMode = menu.Item("igniteMode").GetValue<StringList>().SelectedIndex;
+            Obj_AI_Hero target = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+            
+            var range = Q.Range;
+            if (GetTargetFocus(range) != null)
+                target = GetTargetFocus(range);
+
+            if (target == null)
+                return;
+
+            var dmg = GetComboDamage(target);
+
+            if (useR && R.IsReady() && target.IsValidTarget(R.Range) && R.GetPrediction(target, true).Hitchance >= HitChance.High)
+                R.Cast(target);
+
+            if (igniteMode == 0 && Ignite_Ready() && dmg > target.Health + 50)
+                Use_Ignite(target);
+
+            if (useE && E.IsReady() && target.IsValidTarget(E.Range))
+                E.Cast(packets());
+
+            if(useQ && Q.IsReady() && target.IsValidTarget(Q.Range))
+                Q.CastOnUnit(target, packets());
+
+        }
+
+        private void RMec()
+        {
+            var minHit = menu.Item("useR_Hit").GetValue<Slider>().Value;
+
+            if (!R.IsReady() && minHit == 0)
+                return;
+
+            foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsValidTarget(R.Range)))
+            {
+                var pred = R.GetPrediction(target, true);
+
+                if (pred.Hitchance >= HitChance.High && pred.AoeTargetsHitCount >= minHit)
+                    R.Cast(target);
+            }
+        }
+
+        private void LastHit()
+        {
+            var allMinions = MinionManager.GetMinions(Player.ServerPosition, Q.Range);
+
+            if (Q.IsReady())
+            {
+                foreach (var minion in allMinions)
+                {
+                    if (minion.IsValidTarget() && HealthPrediction.GetHealthPrediction(minion, (int)(Player.Distance(minion) * 1000 / 1400)) < Player.GetSpellDamage(minion, SpellSlot.Q) - 10)
+                    {
+                        Q.CastOnUnit(minion, packets());
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void Farm()
+        {
+            var rangedMinionsE = MinionManager.GetMinions(Player.ServerPosition, E.Range + E.Width, MinionTypes.All, MinionTeam.NotAlly);
+
+            var useQ = menu.Item("UseQFarm").GetValue<bool>();
+            var useE = menu.Item("UseEFarm").GetValue<bool>();
+
+            if (useQ)
+                LastHit();
+
+            if (useE && E.IsReady())
+            {
+                var ePos = E.GetCircularFarmLocation(rangedMinionsE);
+                if (ePos.MinionsHit >= 2)
+                    E.Cast(ePos.Position, packets());
+            }
+        }
+
+        private void CheckKs()
+        {
+            foreach (Obj_AI_Hero target in ObjectManager.Get<Obj_AI_Hero>().Where(x => Player.IsValidTarget(1300)).OrderByDescending(GetComboDamage))
+            {
+                if (Player.Distance(target.ServerPosition) <= E.Range && Player.GetSpellDamage(target, SpellSlot.Q) + Player.GetSpellDamage(target, SpellSlot.E)  > target.Health && Q.IsReady() && E.IsReady())
+                {
+                    E.Cast(packets());
+                    Q.Cast(target, packets());
+                    return;
+                }
+
+                if (Player.Distance(target.ServerPosition) <= Q.Range && Player.GetSpellDamage(target, SpellSlot.Q) > target.Health && Q.IsReady())
+                {
+                    Q.Cast(target, packets());
+                    return;
+                }
+
+                if (Player.Distance(target.ServerPosition) <= E.Range && Player.GetSpellDamage(target, SpellSlot.E) > target.Health && E.IsReady())
+                {
+                    E.Cast(packets());
+                    return;
+                }
+
+                if (Player.Distance(target.ServerPosition) <= R.Range && Player.GetSpellDamage(target, SpellSlot.R) > target.Health && R.IsReady() && menu.Item("R_KS").GetValue<bool>())
+                {
+                    R.Cast(target);
+                    return;
+                }
+            }
+        }
+
+        public override void Game_OnGameUpdate(EventArgs args)
+        {
+
+            if (menu.Item("smartKS").GetValue<bool>())
+                CheckKs();
+
+            if (menu.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                RMec();
+                Combo();
+            }
+            else
+            {
+                if (menu.Item("LastHitKey").GetValue<KeyBind>().Active)
+                    LastHit();
+
+                if (menu.Item("LaneClearActive").GetValue<KeyBind>().Active)
+                    Farm();
+
+                if (menu.Item("HarassActive").GetValue<KeyBind>().Active)
+                    Harass();
+
+                if (menu.Item("HarassActiveT").GetValue<KeyBind>().Active)
+                    Harass();
+            }
+
+            if (IsRecalling())
+                return;
+
+            if (menu.Item("StackE").GetValue<KeyBind>().Active)
+            {
+                if (E.IsReady() && Environment.TickCount - E.LastCastAttemptT >= 9900)
+                    E.Cast(packets());
+            }
+        }
+
+        public override void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base unit, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (!unit.IsMe)
+                return;
+            
+            if (args.SData.Name == "VladimirTidesofBlood")
+            {
+                E.LastCastAttemptT = Environment.TickCount + 250;
+            }
+        }
+
+        public override void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (!menu.Item("W_Gap_Closer").GetValue<bool>()) return;
+
+            if (W.IsReady() && gapcloser.Sender.Distance(Player) < 300)
+                W.Cast(packets());
+        }
+
+        public override void Drawing_OnDraw(EventArgs args)
+        {
+            if (menu.Item("Draw_Disabled").GetValue<bool>())
+                return;
+
+            if (menu.Item("Draw_Q").GetValue<bool>())
+                if (Q.Level > 0)
+                    Utility.DrawCircle(Player.Position, Q.Range, Q.IsReady() ? Color.Green : Color.Red);
+
+            if (menu.Item("Draw_E").GetValue<bool>())
+                if (E.Level > 0)
+                    Utility.DrawCircle(Player.Position, E.Range, E.IsReady() ? Color.Green : Color.Red);
+
+            if (menu.Item("Draw_R").GetValue<bool>())
+                if (R.Level > 0)
+                    Utility.DrawCircle(Player.Position, R.Range, R.IsReady() ? Color.Green : Color.Red);
+        }
+    }
+}
